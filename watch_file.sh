b9e1b59c87228eb6d71bb883d2ea9f855dffdb24
Jan Vincent Wyszynski <jan-vincent.wyszynski@charite.de>
2025-02-08 20:35:49 +0100

    remove watch_file

--- watch_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILE="./supercoop"
-COMMAND="echo 'File has been modified!'"
-inotifywait -m -e modify "$FILE" | while read -r path action file; do
-    echo "File $file has been modified."
-    $COMMAND
-done
